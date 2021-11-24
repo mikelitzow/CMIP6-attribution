bb2aa58d27d862a7e2b9d44a61c7be97af7ee2b5
mikelitzow <mike.litzow@noaa.gov>
2021-11-23 21:35:18 -0900

    Comparing models with ERSST

--- scripts/CMIP6 exploration.R
+++ scripts/CMIP6 exploration.R
@@ -24,12 +24,12 @@ files <- list.files("./model_outputs/")
 # capture the time series of raw temps for area of interest for each file-experiment comparison
 
 # objects for saving dates and temps and file-experiment ID from each realization
-dates <- temps <- experiment.file <- data.frame()
-
+dates <- temps  <- matrix()
+experiment.file <- data.frame()
 
 for(i in 1:length(files)){
 
-  i <- 1
+  # i <- 1
   
   path <- paste("./model_outputs/", files[i], sep="")
   
@@ -40,9 +40,12 @@ for(i in 1:length(files)){
   
   # extract one experiment at a time and save with file name
   
-  for(j in 1:2){
+  # get list of experiments
+  experiments <-  ncvar_get(nc, "experiment", verbose = F)
   
-  j <- 1
+  for(j in 1:length(experiments)){
+  
+  # j <- 1
   
   this.one <- data.frame(file = files[i], 
                          experiment = ncvar_get(nc, "experiment", start = j, count = 1))
@@ -52,11 +55,12 @@ for(i in 1:length(files)){
   
   # extract dates
 
-  
   d <- dates(ncvar_get(nc, "time"), origin = c(1,1,1970))
+  
+  this.name <- paste(files[i], ncvar_get(nc, "experiment", start = j, count = 1), sep = "_")
 
-  these.dates <- data.frame(this.one = d)
-  dates <- rbind(dates, these.dates)
+  these.dates <- data.frame(this.name = as.character(d))
+  dates <- cbind(dates, these.dates)
 
   # extract spatial area
   # 20-68 deg. N, 120-250 deg. E
@@ -80,9 +84,201 @@ for(i in 1:length(files)){
   dimnames(SST) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
 
 
+  drop <- lon > 210 | lon < 202 | lat < 55 | lat > 59
+  SST[,drop] <- NA
+  
+  drop <- lon == 202.5 & lat == 58.5
+  SST[,drop] <- NA
+  
   # and plot 
-  SST.mean <- colMeans(SST)
-  z <- t(matrix(SST.mean,length(y)))  # Re-shape to a matrix with latitudes in columns, longitudes in rows
-  image(x,y,z, col=new.col)
-contour(x, y, z, add=T, col="white")  
-map('world2Hires',fill=F,add=T, lwd=2)
+  # SST.mean <- colMeans(SST)
+  # z <- t(matrix(SST.mean,length(y)))  # Re-shape to a matrix with latitudes in columns, longitudes in rows
+  # image(x,y,z, col=new.col, ylim = c(53, 60), xlim = c(200, 212))
+  # contour(x, y, z, add=T, col="white")
+  # map('world2Hires',fill=F,add=T, lwd=2)
+
+  # get average temperature
+  these.temps <- data.frame(this.name = rowMeans(SST, na.rm = T))
+  
+  temps <- cbind(temps, these.temps)
+  
+  }
+}
+
+# check that all dates are the same
+View(dates)
+
+# just checked visually, but yes, they appear to be!
+
+# load ERSST for comparison - 1950 through 2014
+
+# download.file("https://coastwatch.pfeg.noaa.gov/erddap/griddap/nceiErsstv5.nc?sst[(1950-01-01):1:(2014-12-01T00:00:00Z)][(0.0):1:(0.0)][(54):1:(62)][(200):1:(226)]", "~temp")
+
+
+# load and process SST data
+# nc <- nc_open("~temp")
+
+nc <- nc_open("./data/nceiErsstv5_c83f_8d23_4777.nc")
+
+# process
+
+ncvar_get(nc, "time")   # seconds since 1-1-1970
+raw <- ncvar_get(nc, "time")
+h <- raw/(24*60*60)
+d <- dates(h, origin = c(1,1,1970))
+m <- months(d)
+yr <- years(d)
+
+x <- ncvar_get(nc, "longitude")
+y <- ncvar_get(nc, "latitude")
+
+SST <- ncvar_get(nc, "sst", verbose = F)
+
+SST <- aperm(SST, 3:1)  
+
+SST <- matrix(SST, nrow=dim(SST)[1], ncol=prod(dim(SST)[2:3]))  
+
+# Keep track of corresponding latitudes and longitudes of each column:
+lat <- rep(y, length(x))   
+lon <- rep(x, each = length(y))   
+dimnames(SST) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
+
+# plot to check
+
+# need to drop Bristol Bay cells
+BB <- c("N58E200", "N58E202", "N56E200")
+SST[,BB] <- NA
+
+# and check
+temp.mean <- colMeans(SST, na.rm=T)
+z <- t(matrix(temp.mean,length(y)))  
+image.plot(x,y,z, col=oceColorsPalette(64), xlim=c(195,230), ylim=c(53,62))
+contour(x, y, z, add=T)  
+map('world2Hires',c('Canada', 'usa'), fill=T,xlim=c(130,250), ylim=c(20,66),add=T, lwd=1, col="lightyellow3")
+
+# WGOA for cod/pollock paper
+
+drop <- lon > 210 | lon < 202 | lat < 55 | lat > 59
+wSST <- SST
+wSST[,drop] <- NA
+
+
+# and check
+temp.mean <- colMeans(wSST, na.rm=T)
+z <- t(matrix(temp.mean,length(y)))  
+image.plot(x,y,z, col=oceColorsPalette(64), xlim=c(195,230), ylim=c(53,62))
+map('world2Hires',c('Canada', 'usa'), fill=T,xlim=c(130,250), ylim=c(20,66),add=T, lwd=1, col="lightyellow3")
+
+# calculate monthly mean
+obs.sst <- rowMeans(wSST, na.rm = T)
+
+# and annual observed means
+ann.sst <- tapply(obs.sst, as.numeric(as.character(yr)), mean)
+
+# combine observations and historical simulations in one df
+compare.sst <- data.frame(year = as.numeric(names(ann.sst)),
+                          historical = ann.sst)
+
+
+ggplot(compare.sst, aes(year, historical)) +
+  geom_line()
+
+# now get annual means of historical experiments!
+
+# start with vector for selecting historical simulations
+
+hist <- grep("hist", experiment.file$experiment)
+
+# drop first column of temps
+temps <- temps[,2:ncol(temps)]
+
+# check we have the correct number of temperature outputs
+identical(ncol(temps), nrow(experiment.file)) # looks good
+
+historical.temps <- temps[, hist]
+names(historical.temps) <- experiment.file$file[hist]
+
+# get annual means for each
+
+# get years for one of the runs (they're all the same!)
+path <- paste("./model_outputs/", files[i], sep="")
+
+nc <- nc_open(path)
+
+d <- dates(ncvar_get(nc, "time"), origin = c(1,1,1970))
+
+years <- as.numeric(as.character(years(d)))
+
+
+f <- function(x) tapply(x, years, mean)
+
+ann.hist.temps <- as.data.frame(apply(historical.temps, 2, f))
+
+ann.hist.temps$year <- 1900:2099
+
+compare.sst <- left_join(compare.sst, ann.hist.temps)
+
+# seems to be an offset in units??
+
+# drop year
+compare.sst <- compare.sst[,2:ncol(compare.sst)]
+
+mean.compare <- data.frame(names = as.factor(names(compare.sst)),
+  mean = colMeans(compare.sst))
+
+# looks like some are in K - change
+change <- mean.compare$mean > 200
+
+compare.sst[,change] <- compare.sst[,change] - 273.15
+
+
+mean.compare <- data.frame(names = as.factor(names(compare.sst)),
+                           mean = colMeans(compare.sst))
+
+ggplot(mean.compare, aes(names, mean)) +
+  geom_bar(stat = "identity")
+
+
+# compare bias in climatology and correlation with historical
+
+ersst <- compare.sst[,1]
+historical.runs <- compare.sst[,2:ncol(compare.sst)]
+
+bias <- colMeans(historical.runs) - mean(ersst)
+
+correlation <- cor(historical.runs, ersst)
+
+plot.bias <- data.frame(name = names(bias),
+                        bias = abs(bias)) 
+
+# drop _245; these are repeats I assume with different scenarios
+
+drop <- grep("_245", plot.bias$name)
+plot.bias <- plot.bias[-drop,]
+
+plot.bias$name <- reorder(plot.bias$name, plot.bias$bias)
+
+theme_set(theme_bw())
+
+ggplot(plot.bias, aes(name, bias)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.title.x = element_blank())
+
+ggsave("./figs/model_bias.png", width = 7, height = 5, units = 'in')
+
+
+plot.correlation <- data.frame(name = rownames(correlation),
+                        correlation = correlation) 
+
+drop <- grep("_245", plot.correlation$name)
+plot.correlation <- plot.correlation[-drop,]
+
+plot.correlation$name <- reorder(plot.correlation$name, plot.correlation$correlation)
+
+
+ggplot(plot.correlation, aes(name, correlation)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.title.x = element_blank())
+
+ggsave("./figs/model_correlation.png", width = 7, height = 5, units = 'in')
+
