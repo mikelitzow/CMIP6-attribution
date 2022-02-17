abc6ee3a90c0ac9d65f985bfb3ed219c24a8ec51
mikelitzow <mike.litzow@noaa.gov>
2022-02-17 10:13:19 -0900

    Updated approach - step by step as outlined in issue #3, also completely model-based calculation of probabilities

--- CMIP6/scripts/Bayes FAR time series modeling.R
+++ CMIP6/scripts/Bayes FAR time series modeling.R
@@ -1,41 +1,166 @@
 ## Cod FAR
 ## model the FAR time series to account for model error
 
-library(ggplot2)
-library(mgcv)
 library(rstan)
 library(brms)
 library(bayesplot)
 library(tidyverse)
+library(ncdf4)
+library(zoo)
+library(maps)
+library(mapdata)
+library(chron)
+library(fields)
+library(oce)
 source("./CMIP6/scripts/stan_utils.R")
 
 theme_set(theme_bw())
 
-## Read in data --------------------------------------------
-obs <- read.csv("./CMIP6/summaries/ersst_scaled_GOA_output.csv", row.names=1)
+## annual SST means --------------------
 
-# add 2-yr and 3-yr running means
-obs$two.yr.mean <- zoo::rollmean(obs$anomaly, 2, fill = NA, align = "right")
-obs$three.yr.mean <- zoo::rollmean(obs$anomaly, 3, fill = NA, align = "right")
+# load
+nc <- nc_open("./CMIP6/data/nceiErsstv5_5d38_fa81_4a70.nc")
 
-mod <- read.csv("./CMIP6/summaries/selected_model_scaled_GOA_output.csv", row.names=1)
+# process
+ncvar_get(nc, "time")   # seconds since 1-1-1970
+raw <- ncvar_get(nc, "time")
+h <- raw/(24*60*60)
+d <- dates(h, origin = c(1,1,1970))
+m <- months(d)
+yr <- years(d)
 
-## calculate FAR for observed SST from each model -----------
+x <- ncvar_get(nc, "longitude")
+y <- ncvar_get(nc, "latitude")
 
+SST <- ncvar_get(nc, "sst", verbose = F)
+
+SST <- aperm(SST, 3:1)  
+
+SST <- matrix(SST, nrow=dim(SST)[1], ncol=prod(dim(SST)[2:3]))  
+
+# Keep track of corresponding latitudes and longitudes of each column:
+lat <- rep(y, length(x))   
+lon <- rep(x, each = length(y))   
+dimnames(SST) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
+
+# plot to check
+
+# need to drop Bristol Bay cells
+BB <- c("N58E200", "N58E202", "N56E200")
+SST[,BB] <- NA
+
+# trim spatial area
+
+drop <-  lat < 56 
+SST[,drop] <- NA
+
+drop <-  lat > 55 & lon == 198
+SST[,drop] <- NA
+
+# and check
+temp.mean <- colMeans(SST, na.rm=T)
+z <- t(matrix(temp.mean,length(y)))  
+image.plot(x,y,z, col=oceColorsPalette(64), xlim=c(195,230), ylim=c(52,62))
+map('world2Hires',c('Canada', 'usa'), fill=T,xlim=c(130,250), ylim=c(20,66),add=T, lwd=1, col="lightyellow3")
+
+# calculate monthly mean
+obs.sst <- rowMeans(SST, na.rm = T)
+
+# and annual observed means
+ann.sst <- tapply(obs.sst, as.numeric(as.character(yr)), mean)
+
+## STEP 1 ----------------------------------------------------
+
+# calculate GOA anomalies wrt 1950-1999
+
+# limit to 1950-2021 (years we're confident in)  and scale wrt 1950-1999
+
+climatology.mean <- mean(ann.sst[names(ann.sst) %in% 1950:1999])
+climatology.sd <- sd(ann.sst[names(ann.sst) %in% 1950:1999])
+
+obs.sst <- data.frame(year = 1950:2021,
+                      sc.sst = (ann.sst[names(ann.sst) %in% 1950:2021] - climatology.mean) / climatology.sd)
+
+# combine with 2-year and 3-yr rolling means
+
+obs.sst$sc.sst2 <- rollmean(obs.sst$sc.sst, 2, fill = NA, align = "right") # corresponds to year before and year of ocean entry!
+
+obs.sst$sc.sst3 <- rollmean(obs.sst$sc.sst, 3, fill = NA) # year before, year of, year after ocean entry
+
+plot.sst <- obs.sst %>%
+  pivot_longer(cols = -year)
+
+ggplot(plot.sst, aes(year, value, color = name)) +
+  geom_line() +
+  geom_hline(yintercept = 0)
+
+
+## STEP 2 -----------------------
+# calculate probability wrt 1950-1999 for each model preindustrial and hist_ssp585
+
+# load monthly sst model runs
+
+mod.dat <- read.csv("./CMIP6/summaries/GOA_monthly_sst_piControl_hist585.csv")
+
+# get annual means
+yr <- rep(1850:2099, each = 12)
+
+ff <- function(x) tapply(x, yr, mean)
+
+mod.dat <- mod.dat %>%
+  select(-date)
+
+annual.sst <- apply(mod.dat, 2, ff)
+
+# change to C if needed
+change <- colMeans(annual.sst) > 200
+
+annual.sst[,change] <- annual.sst[,change] - 273.15
+
+# change to anomalies to wrt 1950-1999
+yr <- 1850:2099
+
+ff <- function(x) (x - mean(x[yr %in% 1950:1999])) / sd(x[yr %in% 1950:1999])
+
+annual.anom <- apply(annual.sst, 2, ff)
+  
 # separate preindustrial and hist_ssp585 runs for convenience
+keep <- grep("piControl", names(mod.dat))
 
-preindustrial <- mod %>%
-  dplyr::filter(name == "piControl") 
+preindustrial <- as.data.frame(annual.anom[,keep])
 
-hist_ssp585 <- mod %>%
-  dplyr::filter(name == "hist_ssp585")
+# clean up names
+names(preindustrial) <- str_replace(names(preindustrial), ".nc_piControl", "")
+
+keep <- grep("hist", names(mod.dat))
+
+hist_ssp585 <- as.data.frame(annual.anom[,keep])
+
+# clean up names
+names(hist_ssp585) <- str_replace(names(hist_ssp585), ".nc_hist_ssp585", "")
+
+
+# pivot longer
+preindustrial <- preindustrial %>%
+  mutate(year = 1850:2099) %>%
+  pivot_longer(cols = -year) %>%
+  rename(model = name,
+         goa.sst = value)
+
+hist_ssp585 <- hist_ssp585 %>%
+  mutate(year = 1850:2099) %>%
+  pivot_longer(cols = -year) %>%
+  rename(model = name,
+         goa.sst = value)
+
+
+## STEP 3 ---------------------------------------------
+# calculate preindustrial probability
 
 # get vector of model names
 models <- unique(preindustrial$model)
 
-# object to catch FAR values for each model
-observed.FAR <- data.frame()
-
+preindustrial.prob <- data.frame()
 
 # loop through each model
 for(i in 1:length(models)){
@@ -45,38 +170,38 @@ for(i in 1:length(models)){
   pre.temp <- preindustrial %>% 
     dplyr::filter(model == models[i])
   
-  # add 2-yr and 3-yr running means
-  pre.temp$two.yr.mean <- zoo::rollmean(pre.temp$anomaly, 2, fill = NA, align = "right")
-  pre.temp$three.yr.mean <- zoo::rollmean(pre.temp$anomaly, 3, fill = NA, align = "right")
+  # calculate 2-yr and 3-yr running means
+  pre.temp$two.yr.mean <- zoo::rollmean(pre.temp$goa.sst, 2, fill = NA, align = "right")
+  pre.temp$three.yr.mean <- zoo::rollmean(pre.temp$goa.sst, 3, fill = NA, align = "center") # again - year before, year of, year after
   
+  # set up temporary objects to catch probability
+  preind.1 <- preind.2 <- preind.3 <- NA
  
-  for(j in 1:nrow(obs)){
-    # j <- 120
+  for(j in 1:nrow(obs.sst)){
+    # j <- 1
     
-    # calculate FAR for annual, 2-yr, and 3-yr
-    preind.prob <- sum(pre.temp$anomaly >= obs$anomaly[j])/length(pre.temp$anomaly)
-    obs.prob <- sum(obs$anomaly >=  obs$anomaly[j])/length(obs$anomaly)
-    FAR.1 <- 1 - preind.prob/obs.prob
+    # calculate prob for annual, 2-yr, and 3-yr
+    preind.1[j] <- sum(pre.temp$goa.sst >= obs.sst$sc.sst[j])/length(pre.temp$goa.sst)
 
+    preind.2[j] <- sum(pre.temp$two.yr.mean >= obs.sst$sc.sst2[j], na.rm = T)/length(pre.temp$two.yr.mean)
     
-    preind.prob <- sum(na.omit(pre.temp$two.yr.mean) >= obs$two.yr.mean[j])/length(na.omit(pre.temp$two.yr.mean))
-    obs.prob <- sum(na.omit(obs$two.yr.mean) >=  obs$two.yr.mean[j])/length(na.omit(obs$two.yr.mean))
-    FAR.2 <- 1 - preind.prob/obs.prob
-    
-    preind.prob <- sum(na.omit(pre.temp$three.yr.mean) >= obs$three.yr.mean[j])/length(na.omit(pre.temp$three.yr.mean))
-    obs.prob <- sum(na.omit(obs$three.yr.mean) >=  obs$three.yr.mean[j])/length(na.omit(obs$three.yr.mean))
-    FAR.3 <- 1 - preind.prob/obs.prob
-        
-    observed.FAR <- rbind(observed.FAR, 
-                      data.frame(model = models[i],
-                                 year = obs$year[j],
-                                 FAR.1 = FAR.1,
-                                 FAR.2 = FAR.2,
-                                 FAR.3 = FAR.3))
+    preind.3[j] <- sum(pre.temp$three.yr.mean >= obs.sst$sc.sst3[j], na.rm = T)/length(pre.temp$three.yr.mean)
     
   }
+  
+  # add to df
+  preindustrial.prob <- rbind(preindustrial.prob,
+                              data.frame(model = models[i],
+                                         prob.1yr = preind.1,
+                                         prob.2yr = preind.2,
+                                         prob.3yr = preind.3))
 }
  
+## STEP 4 -------------------------------------
+# Calculate probability at 0.5, 1.0, 1.5, 2.0 warming from hist.585
+
+# load warming timing for each model
+timing <- read.csv("./CMIP6/summaries/")
 
 ggplot(observed.FAR, aes(year, FAR.3, color = model)) +
   geom_line()
