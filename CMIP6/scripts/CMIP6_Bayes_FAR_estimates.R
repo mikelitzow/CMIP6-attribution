156f9597cfefd19cb2fbe10ef9b0700bb8556615
mikelitzow <mike.litzow@noaa.gov>
2022-03-16 09:18:02 -0800

    Removing unsuccessful attempt at binomial model - insufficient computing resources available!

--- CMIP6/scripts/CMIP6_Bayes_FAR_estimates.R
+++ CMIP6/scripts/CMIP6_Bayes_FAR_estimates.R
@@ -34,56 +34,7 @@ hist(FAR$annual.weight, breaks = 8)
 
 sum(is.na(FAR$winter.weight)) # ok
 hist(FAR$winter.weight, breaks = 8)
-# # experimental - logistic model that will then support
-# # posterior estimates of FAR
-# 
-# # load preindustrial and historical outcome for the GOA
-# preindustrial <- read.csv("./CMIP6/summaries/Gulf_of_Alaska_preindustrial_outcomes.csv")
-# historical <- read.csv("./CMIP6/summaries/Gulf_of_Alaska_historical_outcomes.csv")
-# 
-# # combine
-# outcomes <- rbind(preindustrial, historical)
-# 
-# # plot to check
-# ggplot(outcomes, aes(annual.anomaly.1yr, annual.1yr.events)) +
-#   geom_point(alpha = 0.1) +
-#   facet_wrap(~period)
-# 
-# # load model weights (based on ar(1), correlation, bias)
-# weights <- read.csv("./CMIP6/summaries/CMIP6_model_weights_by_region_window.csv")
-# 
-# weights <- weights %>%
-#   filter(window == "annual",
-#          region == "Gulf_of_Alaska") %>%
-#   select(model, scaled.total.weight) %>%
-#   rename(model_weight = scaled.total.weight)
-# 
-# outcomes <- left_join(outcomes, weights)
-# 
-## brms: setup ---------------------------------------------
 
-# # setup variables - model as factor
-# outcomes$model_fac <- as.factor(outcomes$model)
-# 
-# ## fit: brms --------------------------------------
-# 
-# # Define model formula
-# 
-# far_formula <-  bf(annual.1yr.events | weights(model_weight, scale = TRUE) ~
-#                      s(annual.anomaly.1yr, by = period, k = 6) + period + (1 | model_fac))
-# 
-# far_brms <- brm(far_formula,
-#                     data = outcomes,
-#                     family = bernoulli(link = "logit"),
-#                     cores = 4, chains = 4, iter = 3000,
-#                     save_pars = save_pars(all = TRUE),
-#                     control = list(adapt_delta = 0.999, max_treedepth = 15))
-# 
-# saveRDS(far_brms, "./CMIP6/brms_output/Gulf_of_Alaska_binomial.rds")
-
-
-
-# fit to remaining regions
 ## refit GOA
 
 # subset temp.FAR
