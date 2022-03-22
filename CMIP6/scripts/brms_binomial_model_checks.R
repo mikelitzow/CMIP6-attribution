2dfb63fe640bddd9d5574d192ddc39d2a3b3a4ca
mikelitzow <mike.litzow@noaa.gov>
2022-03-22 14:25:14 -0800

    Model evaluation for brms binomial fits

---
+++ CMIP6/scripts/brms_binomial_model_checks.R
@@ -0,0 +1,86 @@
+# check binomial models used to estimate FAR for different "presents"
+library(tidyverse)
+library(brms)
+
+source("./CMIP6/scripts/stan_utils.R")
+
+
+model <- readRDS("./CMIP6/brms_output/North_Pacific_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) 
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model) # wow - 0.99!
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/Eastern_Bering_Sea_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) 
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model) # wow - 0.99!
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/Gulf_of_Alaska_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) 
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/British_Columbia_Coast_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) 
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/Northern_California_Current_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) # a little below 1000
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
+
+
+##
+model <- readRDS("./CMIP6/brms_output/Southern_California_Current_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit) # a little low
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/North_Pacific_wrt_1950-0.5_degrees_warming_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit)# low
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
+
+##
+model <- readRDS("./CMIP6/brms_output/Eastern_Bering_Sea_wrt_1950-0.5_degrees_warming_binomial2.rds") # checks out
+
+check_hmc_diagnostics(model$fit)
+neff_lowest(model$fit)# low
+rhat_highest(model$fit)
+summary(model)
+bayes_R2(model)
+trace_plot(model$fit)
