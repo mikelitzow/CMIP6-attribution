1cb3d02e2f58fb0514e3d234f0859379f8d5444e
mikelitzow <mike.litzow@noaa.gov>
2022-03-24 21:51:56 -0800

    Plotted and summarized warming timing for ssp585

--- CMIP6/scripts/CMIP6 Bayes N. Pacific warming rate.R
+++ CMIP6/scripts/CMIP6 Bayes N. Pacific warming rate.R
@@ -67,3 +67,67 @@ ppc_dens_overlay(y = y, yrep = yrep_warming_brm[sample(nrow(yrep_warming_brm), 2
 pdf("./figs/trace_warming_brm.pdf", width = 6, height = 4)
 trace_plot(warming_brm$fit)
 dev.off()
+
+
+## SST anomaly predictions #### 95% CI
+ce1s_1 <- conditional_effects(warming_brm, effect = "year", re_formula = NA,
+                              probs = c(0.025, 0.975))
+
+dat_ce <- ce1s_1$year %>%
+  select(year, estimate__, lower__, upper__) %>%
+  mutate(Source = "Modeled warming")
+
+# add observations
+ersst <- read.csv("./CMIP6/summaries/North_Pacific_ersst_1854-2021.csv")
+ersst$warming.wrt.1854.1949 <- ersst$annual.unsmoothed - mean(ersst$annual.unsmoothed[ersst$year %in% 1854:1949])
+
+obs <- ersst %>%
+  select(year, warming.wrt.1854.1949) %>%
+  rename(estimate__ = warming.wrt.1854.1949) %>%
+  mutate(Source = "Actual temperature",
+         lower__ = NA,
+         upper__ = NA)
+
+dat_ce <- rbind(dat_ce, obs)
+
+dat_ce$order <- if_else(dat_ce$Source == "Actual temperature", 2, 1)
+
+dat_ce$Source <- reorder(dat_ce$Source, dat_ce$order)
+
+cb <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+ggplot(dat_ce) +
+  aes(x = year, y = estimate__, color = Source) +
+  geom_line() +
+  geom_ribbon(data = filter(dat_ce, Source == "Modeled warming"), 
+              aes(x = year, ymin = lower__, ymax = upper__), alpha = 0.15, lty = 0) +
+  theme_bw() +
+  theme(axis.title = element_blank()) +
+  scale_color_manual(values = cb[c(2,4)])
+
+
+ggsave("./CMIP6/figs/modeled_observed_warming.png", width = 5, height = 3)
+
+
+# get predicted warming for each year
+new.dat <- data.frame(year = 1950:2100,
+                      model = NA)
+
+pred <- posterior_epred(warming_brm, newdata = new.dat, re_formula = NA)
+
+new.dat$pred_mean <- colMeans(pred) 
+new.dat$lower = apply(pred, 2, quantile, probs = 0.025)
+new.dat$upper = apply(pred, 2, quantile, probs = 0.975)
+
+ggplot(new.dat, aes(year, pred_mean)) +
+  geom_line()
+
+View(new.dat)
+
+## warming timings under ssp585
+# 2003 is the best timing for 0.5 degrees warming
+# 2019 is the best timing for 1.0 degrees warming
+# 2021 warming is c. 1.09 degrees
+# 2033 warming is 1.5 degrees
+# 2045 warming is 2.0 degrees
+# 
