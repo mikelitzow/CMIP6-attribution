4956b4497ec10faab7b9cc987c79c05a4c05c497
mikelitzow <mike.litzow@noaa.gov>
2022-04-04 11:06:41 -0800

    modeling FAR for 3-yr running mean SST

---
+++ CMIP6/scripts/FAR_time_series_plots_rolling_window_models_3yr_running_mean.R
@@ -0,0 +1,90 @@
+# plot FAR time series for each region and each warming level
+# using 15-year rolling windows of observed warming ranges to set "present"
+
+library(tidyverse)
+library(brms)
+
+theme_set(theme_bw())
+
+# load region names
+regions <- read.csv("./CMIP6/summaries/clean_region_names.csv")
+regions <- regions[1:6,1]
+
+# load ERSST anomalies
+ersst.anom <- read.csv("./CMIP6/summaries/regional_north_pacific_ersst_anomaly_time_series.csv")
+
+far_pred <- data.frame()
+
+for(i in 1:length(regions)){ # loop through regions
+ 
+  # i <- 1
+  
+  # subset ersst.anom
+  ersst.temp <- ersst.anom %>%
+    filter(region == regions[i]) %>%
+    select(year, annual.anomaly.three.yr.running.mean) %>%
+    rename(annual.anomaly.3yr = annual.anomaly.three.yr.running.mean)
+  
+  # and drop NAs
+  keep <- !is.na(ersst.temp$annual.anomaly.3yr)
+  ersst.temp <- ersst.temp[keep,]
+  
+  # load regional model
+  mod <- readRDS(paste("./CMIP6/brms_output/", regions[i], "_3yr_mean_annual_sst_rolling_window_binomial2.rds", sep = ""))
+  
+  ## setup new data
+  nd <- data.frame(period = c("historical", "preindustrial"),
+                   ersst.year = rep(ersst.temp$year, each = 2),
+                   annual.anomaly.3yr = rep(ersst.temp$annual.anomaly.3yr, each = 2),
+                   N = 1000,
+                   model_fac = NA)
+  
+  nd_pre <- nd[nd$period == "preindustrial", ]
+  nd_his <- nd[nd$period == "historical", ]
+  
+  ## make predictions
+  ## exclude random effects for model_fac
+  pre_pp <- posterior_epred(mod, newdata = nd_pre, re_formula = NA)
+  his_pp <- posterior_epred(mod, newdata = nd_his, re_formula = NA)
+  
+  ## Calc probabilities
+  ## These are our posterior probabilities to use for FAR calculation
+  pre_prob <- pre_pp / unique(nd$N)
+  his_prob <- his_pp / unique(nd$N)
+  
+  
+  ## Calc FAR
+  far <- 1 - (pre_prob / his_prob)
+  range(far, na.rm = TRUE)
+  
+  
+  far_pred <- rbind(far_pred,
+                    data.frame(region = regions[i],
+                              year = nd_pre$ersst.year,
+                              prob = apply(far, 2, mean),
+                              lower = apply(far, 2, quantile, probs = 0.025),
+                              upper = apply(far, 2, quantile, probs = 0.975)))
+  
+  
+} # close i loop
+  
+# reorder areas 
+region.order <- data.frame(region = regions,
+                           order = 1:6)
+
+far_pred <- left_join(far_pred, region.order)
+
+far_pred$region <- reorder(far_pred$region, far_pred$order)
+
+  g <- ggplot(far_pred) +
+    geom_hline(yintercept = 0, color = "grey50", linetype = 2) +
+    geom_line(aes(x = year, y = prob), size = 0.25, color = "red") +
+    geom_ribbon(aes(x = year, ymin = lower, ymax = upper), alpha = 0.15) +
+    facet_wrap(~region) +
+    ylab("Fraction of Attributable Risk") +
+    theme(axis.title.x = element_blank())
+  
+  print(g)
+  
+ggsave("./CMIP6/figs/FAR_3yr_mean_annual_sst_rolling_window_time_series.png", height = 5, width = 8, units = 'in')  
+  
