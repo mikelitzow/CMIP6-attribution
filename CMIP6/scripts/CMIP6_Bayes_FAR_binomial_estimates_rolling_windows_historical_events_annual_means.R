9a8b773237fb741d215cff18bf138f526a35d513
mikelitzow <mike.litzow@noaa.gov>
2023-03-04 17:09:18 -0900

    Model diagnostics

--- CMIP6/scripts/CMIP6_Bayes_FAR_binomial_estimates_rolling_windows_historical_events_annual_means.R
+++ CMIP6/scripts/CMIP6_Bayes_FAR_binomial_estimates_rolling_windows_historical_events_annual_means.R
@@ -15,7 +15,7 @@ theme_set(theme_bw())
 regions <- read.csv("./CMIP6/summaries/clean_region_names.csv")
 regions <- regions[1:6,1]
 
-for(i in 1:length(regions)){
+for(i in 3:length(regions)){
     # i <-5
 ## load preindustrial and historical outcomes
 preindustrial <- read.csv(paste("./CMIP6/summaries/", regions[i], "_preindustrial_outcomes.csv", sep = ""))
@@ -87,7 +87,95 @@ saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "annual_sst_rolling
  }
 ## ~2.5 hours run time
 
-far_brms2 <- readRDS("./CMIP6/brms_output/Gulf_of_Alaska_binomial2.rds")
+
+## check model diagnostics
+
+# reload region names
+regions <- read.csv("./CMIP6/summaries/clean_region_names.csv")
+regions <- regions[1:6,1]
+
+for(i in 1:length(regions)){
+
+    # i <- 1
+    
+    far_brms2 <- readRDS(paste("./CMIP6/brms_output/", regions[i], "annual_sst_rolling_window_binomial2.rds", sep = ""))
+
+    print(paste("Region #", i, ": ", regions[i], sep = ""))
+    print("")
+    check_hmc_diagnostics(far_brms2$fit)
+    print("")
+    print(neff_lowest(far_brms2$fit))
+    print("")
+    print(rhat_highest(far_brms2$fit))
+}
+
+# North Pacific model has one divergence - refit with higher adapt_delta
+i <- 1
+preindustrial <- read.csv(paste("./CMIP6/summaries/", regions[i], "_preindustrial_outcomes.csv", sep = ""))
+
+historical <- read.csv(paste("./CMIP6/summaries/", regions[i], "_historical_outcomes_rolling_window.csv", sep = ""))
+
+# combine
+outcomes <- rbind(preindustrial, historical)
+
+# load model weights (based on ar(1), correlation, bias)
+weights <- read.csv("./CMIP6/summaries/normalized_CMIP6_weights.csv")
+
+weights <- weights %>%
+    filter(region == regions[i]) %>%
+    select(model, normalized_weight) %>%
+    rename(model_weight = normalized_weight)
+
+outcomes <- left_join(outcomes, weights) %>%
+    select(model, model_weight, period, region, ersst.year, annual.anomaly.1yr, annual.1yr.events)
+
+# # drop NAs due to smoothing
+# drop <- is.na(outcomes$annual.anomaly.3yr)
+# outcomes <- outcomes[!drop,]
+
+## brms: setup ---------------------------------------------
+
+library(data.table)
+dt <- as.data.table(outcomes)
+dt[model == "ACCESS-CM2" & period == "historical", .(N = .N), by = .(period, model, ersst.year)]
+dt[model == "ACCESS-CM2" & period == "historical" & ersst.year == 2017, ]
+
+prop <- dt[ , .(count = sum(annual.1yr.events, na.rm = T),
+                N = .N,
+                annual.anomaly.1yr = unique(annual.anomaly.1yr),
+                model_weight = unique(model_weight)),
+            by = .(period, model, ersst.year)]
+prop[ , prop := count / N]
+prop[ , model_fac := as.factor(model)]
+prop[ , year_fac := as.factor(ersst.year)]
+
+
+head(prop[period == "preindustrial", ])
+head(prop[period == "historical", ])
+
+g <- ggplot(prop) +
+    geom_point(aes(x = year_fac, y = prop, color = model_fac)) +
+    facet_wrap( ~ period) +
+    theme(legend.position="none")
+print(g)
+
+
+form <-  bf(count | trials(N) + weights(model_weight, scale = TRUE) ~
+                period + s(annual.anomaly.1yr, by = period, k = 6) +
+                s(ersst.year, by = period, k = 6) + (1 | model_fac))
+
+far_brms2 <- brm(form,
+                 data = prop,
+                 family = binomial(link = "logit"),
+                 seed = 1234,
+                 cores = 4, chains = 4, iter = 4000,
+                 save_pars = save_pars(all = TRUE),
+                 control = list(adapt_delta = 0.9999, max_treedepth = 15))
+
+# saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "_binomial2.rds", sep = ""))
+
+saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "annual_sst_rolling_window_binomial2.rds", sep = ""))
+##
 
 ce <- conditional_effects(far_brms2)
 plot(ce, ask = FALSE)
