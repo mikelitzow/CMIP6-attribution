5daeba3d5cee3beb4d2071587d744cadcf9db5db
mikelitzow <mike.litzow@noaa.gov>
2023-03-04 17:12:56 -0900

    Cleaned up

--- CMIP6/scripts/CMIP6_Bayes_FAR_binomial_estimates_rolling_windows_historical_events_3yr_annual_means.R
+++ CMIP6/scripts/CMIP6_Bayes_FAR_binomial_estimates_rolling_windows_historical_events_3yr_annual_means.R
@@ -26,7 +26,7 @@ historical <- read.csv(paste("./CMIP6/summaries/", regions[i], "_historical_outc
 # combine
 outcomes <- rbind(preindustrial, historical)
 
-# load model weights (based on ar(1), correlation, bias)
+# load model weights 
 weights <- read.csv("./CMIP6/summaries/normalized_CMIP6_weights.csv")
 
 weights <- weights %>%
@@ -35,7 +35,7 @@ weights <- weights %>%
    rename(model_weight = normalized_weight)
 
 outcomes <- left_join(outcomes, weights) %>%
-    select(model, model_weight, period, region, ersst.year, annual.anomaly.1yr, annual.1yr.events)
+    select(model, model_weight, period, region, ersst.year, annual.anomaly.3yr, annual.3yr.events)
 
 # # drop NAs due to smoothing
 # drop <- is.na(outcomes$annual.anomaly.3yr)
@@ -50,7 +50,7 @@ dt[model == "ACCESS-CM2" & period == "historical" & ersst.year == 2017, ]
 
 prop <- dt[ , .(count = sum(annual.3yr.events, na.rm = T),
                 N = .N,
-                annual.anomaly.1yr = unique(annual.anomaly.1yr),
+                annual.anomaly.3yr = unique(annual.anomaly.3yr),
                 model_weight = unique(model_weight)),
            by = .(period, model, ersst.year)]
 prop[ , prop := count / N]
@@ -69,7 +69,7 @@ print(g)
 
 
 form <-  bf(count | trials(N) + weights(model_weight, scale = TRUE) ~
-            period + s(annual.anomaly.1yr, by = period, k = 6) +
+            period + s(annual.anomaly.3yr, by = period, k = 6) +
             s(ersst.year, by = period, k = 6) + (1 | model_fac))
 
 far_brms2 <- brm(form,
@@ -82,7 +82,7 @@ far_brms2 <- brm(form,
 
 # saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "_binomial2.rds", sep = ""))
 
-saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "mean_annual_sst_rolling_window_binomial2.rds", sep = ""))
+saveRDS(far_brms2, paste("./CMIP6/brms_output/", regions[i], "_3yr_sst_rolling_window_binomial2.rds", sep = ""))
 
  }
 ## ~2.5 hours run time
@@ -92,7 +92,7 @@ far_brms2 <- readRDS("./CMIP6/brms_output/Gulf_of_Alaska_binomial2.rds")
 ce <- conditional_effects(far_brms2)
 plot(ce, ask = FALSE)
 
-ce2 <- conditional_effects(far_brms2, effect = "annual.anomaly.1yr:period", re_formula = NA)
+ce2 <- conditional_effects(far_brms2, effect = "annual.anomaly.3yr:period", re_formula = NA)
 plot(ce2)
 head(ce2[[1]])
 tail(ce2[[1]])
@@ -101,7 +101,7 @@ tail(ce2[[1]])
 
 ## setup new data
 x <- ce2[[1]]
-nd <- x[ , c("period", "annual.anomaly.1yr", "N", "model_fac")]
+nd <- x[ , c("period", "annual.anomaly.3yr", "N", "model_fac")]
 nd$N <- 1000
 nd_pre <- nd[nd$period == "preindustrial", ]
 nd_his <- nd[nd$period == "historical", ]
@@ -125,20 +125,20 @@ range(his_prob)
 
 ## Plot conditional effects
 pre_pred <- data.frame(period = "preindustrial",
-                       annual.anomaly.1yr = nd_pre$annual.anomaly.1yr,
+                       annual.anomaly.3yr = nd_pre$annual.anomaly.3yr,
                        prob = apply(pre_prob, 2, mean),
                        lower = apply(pre_prob, 2, quantile, probs = 0.025),
                        upper = apply(pre_prob, 2, quantile, probs = 0.975))
 his_pred <- data.frame(period = "historical",
-                       annual.anomaly.1yr = nd_his$annual.anomaly.1yr,
+                       annual.anomaly.3yr = nd_his$annual.anomaly.3yr,
                        prob = apply(his_prob, 2, mean),
                        lower = apply(his_prob, 2, quantile, probs = 0.025),
                        upper = apply(his_prob, 2, quantile, probs = 0.975))
 pred <- rbind(pre_pred, his_pred)
 
 g <- ggplot(pred) +
-    geom_line(aes(x = annual.anomaly.1yr, y = prob, color = period), size = 0.8) +
-    geom_ribbon(aes(x = annual.anomaly.1yr, ymin = lower, ymax = upper, fill = period), alpha = 0.15) +
+    geom_line(aes(x = annual.anomaly.3yr, y = prob, color = period), size = 0.8) +
+    geom_ribbon(aes(x = annual.anomaly.3yr, ymin = lower, ymax = upper, fill = period), alpha = 0.15) +
     scale_color_manual(values = c("tomato", "steelblue")) +
     scale_fill_manual(values = c("tomato", "steelblue"))
 print(g)
@@ -150,13 +150,13 @@ far <- 1 - (pre_prob / his_prob)
 range(far, na.rm = TRUE)
 
 
-far_pred <- data.frame(annual.anomaly.1yr = nd_pre$annual.anomaly.1yr,
+far_pred <- data.frame(annual.anomaly.3yr = nd_pre$annual.anomaly.3yr,
                        prob = apply(far, 2, mean),
                        lower = apply(far, 2, quantile, probs = 0.025),
                        upper = apply(far, 2, quantile, probs = 0.975))
 
 g <- ggplot(far_pred) +
     geom_hline(yintercept = 0, color = "grey50", linetype = 2) +
-    geom_line(aes(x = annual.anomaly.1yr, y = prob), size = 0.8) +
-    geom_ribbon(aes(x = annual.anomaly.1yr, ymin = lower, ymax = upper), alpha = 0.15)
+    geom_line(aes(x = annual.anomaly.3yr, y = prob), size = 0.8) +
+    geom_ribbon(aes(x = annual.anomaly.3yr, ymin = lower, ymax = upper), alpha = 0.15)
 print(g)
